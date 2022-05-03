abf1725aae14fe75e266a445e43252d0bde2d327
Derek Pedersen <derekpedersen.com@gmail.com>
2022-05-03 16:24:17 -0700

    Apparently if you want to get a text below a mat-icon you need to write a custom component

--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -14,28 +14,35 @@ export class NavbarComponent implements OnInit {
     private sanitizer: DomSanitizer) {
 
     matIconRegistry
-      .addSvgIcon('linkedin-box',
-        sanitizer.bypassSecurityTrustResourceUrl('/assets/icons/linkedin-box.svg'))
       .addSvgIcon('bitbucket',
         sanitizer.bypassSecurityTrustResourceUrl('/assets/icons/bitbucket.svg'))
-      .addSvgIcon('file-pdf-box',
-        sanitizer.bypassSecurityTrustResourceUrl('/assets/icons/file-pdf-box.svg'))
-      .addSvgIcon('twitter-box',
-        sanitizer.bypassSecurityTrustResourceUrl('/assets/icons/twitter-box.svg'))
-      .addSvgIcon('github-box',
-        sanitizer.bypassSecurityTrustResourceUrl('/assets/icons/github-box.svg'))
-      .addSvgIcon('microsoft',
-        sanitizer.bypassSecurityTrustResourceUrl('/assets/icons/microsoft.svg'))
       .addSvgIcon('codefights',
         sanitizer.bypassSecurityTrustResourceUrl('/assets/icons/codefights.svg'))
-      .addSvgIcon('udemy',
-        sanitizer.bypassSecurityTrustResourceUrl('/assets/icons/udemy.svg'))
-      .addSvgIcon('stackoverflow',
-        sanitizer.bypassSecurityTrustResourceUrl('/assets/icons/stackoverflow.svg'))
+      .addSvgIcon('docker',
+        sanitizer.bypassSecurityTrustResourceUrl('/assets/icons/docker.svg'))
+      .addSvgIcon('file-pdf-box',
+        sanitizer.bypassSecurityTrustResourceUrl('/assets/icons/file-pdf-box.svg'))
+      .addSvgIcon('github-box',
+        sanitizer.bypassSecurityTrustResourceUrl('/assets/icons/github-box.svg'))
       .addSvgIcon('hackerrank',
         sanitizer.bypassSecurityTrustResourceUrl('/assets/icons/hackerrank.svg'))
-      .addSvgIcon('docker',
-        sanitizer.bypassSecurityTrustResourceUrl('/assets/icons/docker.svg'));
+      .addSvgIcon('hammer',
+        sanitizer.bypassSecurityTrustResourceUrl('/assets/icons/hammer.svg'))
+      .addSvgIcon('jenkins',
+        sanitizer.bypassSecurityTrustResourceUrl('/assets/icons/jenkins.svg'))
+      .addSvgIcon('jira',
+        sanitizer.bypassSecurityTrustResourceUrl('/assets/icons/jira.svg'))
+      .addSvgIcon('linkedin-box',
+        sanitizer.bypassSecurityTrustResourceUrl('/assets/icons/linkedin-box.svg'))
+      .addSvgIcon('microsoft',
+        sanitizer.bypassSecurityTrustResourceUrl('/assets/icons/microsoft.svg'))
+      .addSvgIcon('stackoverflow',
+        sanitizer.bypassSecurityTrustResourceUrl('/assets/icons/stackoverflow.svg'))
+      .addSvgIcon('twitter-box',
+        sanitizer.bypassSecurityTrustResourceUrl('/assets/icons/twitter-box.svg'))
+      .addSvgIcon('udemy',
+        sanitizer.bypassSecurityTrustResourceUrl('/assets/icons/udemy.svg'))
+      ;
   }
 
   ngOnInit() {
